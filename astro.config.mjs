db677d474ab7816e8a48473be309a76b887192b6
Daniel <jesusdanielv16@gmail.com>
2026-06-23 12:25:09 -0500

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://studykit.jesusdanielv16.workers.dev',
+  integrations: [sitemap()],
+});
